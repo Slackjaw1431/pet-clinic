a003bddcb8fcfe7cced6cfab5d3623351aeab213
Slackjaw1431 <55801287+Slackjaw1431@users.noreply.github.com>
2023-02-12 04:49:04 -0500

    Refactoring to make Ids auto managed by Sprint

--- pet-clinic-web/src/main/java/com/tmportfolio/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/tmportfolio/petclinic/bootstrap/DataLoader.java
@@ -23,28 +23,24 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Mike");
         owner1.setLastName("West");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Bill");
         owner2.setLastName("Blue");
 
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Mrs.");
         vet1.setLastName("Cat");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Mrs.");
         vet2.setLastName("Dog");
 
