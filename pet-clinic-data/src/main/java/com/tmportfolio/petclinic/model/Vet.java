3330e2267b51d9b623690181dd04c33e21ccf3b5
Slackjaw1431 <55801287+Slackjaw1431@users.noreply.github.com>
2023-02-17 17:50:15 -0500

    Adding and connceted Vet speciality and pet and owner fields

--- pet-clinic-data/src/main/java/com/tmportfolio/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/tmportfolio/petclinic/model/Vet.java
@@ -1,6 +1,10 @@
 package com.tmportfolio.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+
+    private Set<Speciality> specialities;
 
     public Vet(){
         super();
@@ -10,5 +14,12 @@ public class Vet extends Person{
         super(firstName, lastName);
     }
 
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 
 }
