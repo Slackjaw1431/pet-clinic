86111508a09abb59a2706605c70a37a691ec6f4d
Slackjaw1431 <55801287+Slackjaw1431@users.noreply.github.com>
2023-03-26 00:13:59 -0400

    Display pet type name

--- pet-clinic-data/src/main/java/com/tmportfolio/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/tmportfolio/petclinic/model/PetType.java
@@ -17,11 +17,8 @@ public class PetType extends BaseEntity{
     @Column(name = "name")
     private String name;
 
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
+    @Override
+    public String toString() {
+        return name;
+    }
 }
